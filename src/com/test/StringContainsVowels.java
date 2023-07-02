15b893e831f2049346e159a1d92a032f5b1e5540
theabdul97@gmail.com <theabdul97@gmail.com>
2023-07-02 07:45:20 +0530

    StringContainsVowelsUsingRegExp

---
+++ src/com/test/StringContainsVowels.java
@@ -0,0 +1,11 @@
+package com.test;
+public class StringContainsVowels {
+
+	public static void main(String[] args) {
+		System.out.println(stringContainsVowels("Hello"));
+		System.out.println(stringContainsVowels("TV"));
+	}
+	private static boolean stringContainsVowels(String input) {
+		return input.toLowerCase().matches(".*[aeiou].*");
+	}
+}
