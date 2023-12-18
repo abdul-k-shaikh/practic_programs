5c581c006494a163042bc06b890ce94163e9235c
theabdul97@gmail.com <theabdul97@gmail.com>
2023-12-18 23:16:42 +0530

    onlyOddNumberUsingParallelStream

--- src/com/test/collections/onlyOddNumbers.java
+++ src/com/test/collections/onlyOddNumbers.java
@@ -3,7 +3,6 @@ package com.test.collections;
 import java.util.ArrayList;
 
 public class onlyOddNumbers {
-
 	public static void main(String[] args) {
 		ArrayList<Integer> al = new ArrayList<>();
 		al.add(3);
@@ -12,21 +11,24 @@ public class onlyOddNumbers {
 		al.add(7);
 		boolean res = onlyOddNumbers(al);
 		System.out.println("true means list contains odd numbers :"+res);
-
 	}
+	// we can use parallel stream for faster processing
 
 	private static boolean onlyOddNumbers(ArrayList<Integer> al) {
-		System.out.println("list si al"+ al);
-		for (Integer i : al) {
-			System.out.println(i);
-			if(i%2==0) {
-				return false;
-			}
-		}
-		
-		return true;
+		return al.parallelStream().anyMatch(x->x%2!=0);
 	}
 
+	/*
+	 * private static boolean onlyOddNumbers(ArrayList<Integer> al) {
+	 * System.out.println("list si al"+ al); for (Integer i : al) {
+	 * System.out.println(i); if(i%2==0) { return false; } }
+	 * 
+	 * return true; }
+	 */
+	
+	
+
+	
 	
 
 }
