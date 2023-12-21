aabdd5645722418bb449c4ed17c39f6c1fbbc4f2
theabdul97@gmail.com <theabdul97@gmail.com>
2023-12-21 22:44:24 +0530

    sortAnArray

---
+++ src/com/test/array/SortAnArray.java
@@ -0,0 +1,12 @@
+package com.test.array;
+
+import java.util.Arrays;
+
+public class SortAnArray {
+
+	public static void main(String[] args) {
+		int a[] = {1, 2, 3, -1, -2, 4};
+		Arrays.sort(a);
+		System.out.println("after sorting:"+ Arrays.toString(a));
+	}
+}
