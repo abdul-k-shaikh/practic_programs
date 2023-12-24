7c79a30f77882ee275830458d3ed9efe51b575b2
theabdul97@gmail.com <theabdul97@gmail.com>
2023-12-24 22:34:35 +0530

    PalindromNumber

---
+++ src/com/test/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		int a = 22122;
+		int temp = a;
+		//int n;
+		int rev=0;
+		while(a>0) {
+			rev = rev*10;
+			rev+=a%10;
+			a=a/10;
+		}
+//		System.out.println("rev:"+rev +" a: "+a);
+       if(rev==temp) {
+    	   System.out.println("P");
+       }
+	}
+
+}
--- src/com/test/array/SortAnArr.java
+++ src/com/test/array/SortAnArr.java
@@ -1,22 +1,20 @@
 package com.test.array;
 
-import java.util.Arrays;
-
 public class SortAnArr {
 
 	public static void main(String[] args) {
-		int a[] = {3,2,1,4,0,};
-		//int tem =0;
+		int a[] = { 3, 2, 1, 4, 0, };
+		// int tem =0;
 		for (int i = 0; i < a.length; i++) {
-			for (int j = i+1; j < a.length; j++) {
-				
-				if(a[i]>a[j]) {
-					int temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;
+			for (int j = i + 1; j < a.length; j++) {
+
+				if (a[i] > a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
 //					System.out.println(temp);
 				}
-				
+
 			}
 			System.out.println(a[i]);
 		}
