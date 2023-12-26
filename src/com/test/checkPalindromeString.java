d922ae4eea86da72944efe78f471db59e6684624
theabdul97@gmail.com <theabdul97@gmail.com>
2023-12-26 21:34:31 +0530

    palindrome

---
+++ src/com/test/checkPalindromeString.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class checkPalindromeString {
+
+	public static void main(String[] args) {
+		String input = "naman";
+		boolean res = checkPalindromeString(input);
+		System.out.println("is it pallindrome: "+ res);
+
+	}
+
+	static boolean checkPalindromeString(String input) {
+		boolean res1 = true;
+		int len = input.length();
+		for (int i = 0; i < len; i++) {
+			if (input.charAt(i) != input.charAt(len - i - 1)) {
+				res1 = false;
+				break;
+			}
+		}
+		return res1;
+
+	}
+
+}
