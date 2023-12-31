7e5a06c5567fc6c4c0ab4eb1aa228b8af986e7ae
theabdul97@gmail.com <theabdul97@gmail.com>
2023-12-31 22:54:25 +0530

    pattern8

--- src/com/test/pattern/Pattern8.java
+++ src/com/test/pattern/Pattern8.java
@@ -3,7 +3,14 @@ package com.test.pattern;
 public class Pattern8 {
 
 	public static void main(String[] args) {
-		for(int i=0)
+		System.out.println("pattern");
+		int n=8;
+		for(int i=1;i<8;i++) {
+			for(int j=i;j<=n; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
