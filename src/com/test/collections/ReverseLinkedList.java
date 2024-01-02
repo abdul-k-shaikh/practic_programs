ce9977d67df0ca1c118ae54253daef71aa2fa2c4
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-02 23:45:45 +0530

    LL_rev

--- src/com/test/collections/ReverseLinkedList.java
+++ src/com/test/collections/ReverseLinkedList.java
@@ -1,24 +1,16 @@
 package com.test.collections;
 
 import java.util.LinkedList;
-
+//LL reverse using inbuilt methods
 public class ReverseLinkedList {
-
 	public static void main(String[] args) {
-		LinkedList<Integer>ll = new LinkedList<>();
+		LinkedList<Integer> ll = new LinkedList<>();
 		ll.add(1);
 		ll.add(3);
 		ll.add(6);
-		
 		System.out.println(ll);
-		LinkedList<Integer>l2 = new LinkedList<>();
-		
+		LinkedList<Integer> l2 = new LinkedList<>();
 		ll.descendingIterator().forEachRemaining(l2::add);
-		
-		System.out.println("rev ll is :"+l2);
-		
-		
-
+		System.out.println("rev ll is :" + l2);
 	}
-
 }
