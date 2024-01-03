99702a3f0f52cfad704d6afecedaff89d77ff331
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-03 23:32:29 +0530

    PyramidPattern

---
+++ src/com/test/pattern/Pattern9.java
@@ -0,0 +1,16 @@
+package com.test.pattern;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		int n = 6;
+		for(int i=1; i<n; i++) {
+			for(int j=i; j<=1; j++) {
+				System.out.println(" ");
+			}
+			System.out.println("*");
+		}
+
+	}
+
+}
