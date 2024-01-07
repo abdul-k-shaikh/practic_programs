301aa2e0f48f0691f77aafc969a24846dcbcd4fd
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-07 23:10:31 +0530

    sumOfarray

--- src/com/test/array/SumOfArray2.java
+++ src/com/test/array/SumOfArray2.java
@@ -5,10 +5,16 @@ public class SumOfArray2 {
 	public static void main(String[] args) {
 		int a[] = {1,2,3,4};
 		int sum=0;
-		for(int i=0; i<a.length; i++) {
+//		for(int i=0; i<a.length; i++) {
+//			sum+=a[i];
+//		}
+		
+		for(Integer i : a) {
+			System.out.println();
 			sum+=a[i];
+			System.out.println("sum is :"+ sum);
 		}
-		System.out.println("sum is :"+ sum);
+		
 	}
 
 }
