012c872f291ad1b6ff5ebab23537d52ba7eb08fa
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-11 23:49:23 +0530

    2ndLargest

--- src/com/test/hackerrank/SecondLargestElement2.java
+++ src/com/test/hackerrank/SecondLargestElement2.java
@@ -1,27 +1,23 @@
 package com.test.hackerrank;
 
 public class SecondLargestElement2 {
-
 	public static void main(String[] args) {
-		int a[] = {1,3,2,45,67};
+		int a[] = { 1, 3, 2, 12 };
 		int res = SecondLargest(a);
-		System.out.println("2nd highest element is :"+ res);
-
+		System.out.println("2nd highest element is :" + res);
 	}
 
 	private static int SecondLargest(int[] a) {
 		int Higest = Integer.MIN_VALUE;
 		int SecondHighest = Integer.MIN_VALUE;
-		for(int i=0; i<a.length; i++) {
-			if(a[i]>Higest) {
-				SecondHighest=Higest;
-				Higest=i;
-			}
-			else if(a[i]<Higest) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > Higest) {
+				SecondHighest = Higest;
+				Higest = a[i];
+			} else if (a[i] < Higest) {
 				SecondHighest = a[i];
 			}
 		}
 		return SecondHighest;
 	}
-
 }
