593ba8ba2e62ae4664bb4a83f099a446f136bb93
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-13 23:00:23 +0530

    2ndlargest

---
+++ src/com/test/hackerrank/SecondLarEle.java
@@ -0,0 +1,27 @@
+package com.test.hackerrank;
+
+public class SecondLarEle {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,6,9};
+		int res = SecondLargest(a);
+		System.out.println("res :" + res);
+
+	}
+
+	private static int SecondLargest(int[] a) {
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+		for(int i =0; i<a.length; i++) {
+			if(a[i]>largest) {
+				secondLargest=largest;
+				largest = a[i];
+			}
+			else if(a[i]<largest) {
+				secondLargest=a[i];
+			}
+		}
+		return secondLargest;
+	}
+
+}
