ea5cf4042be6e99ab702d4a6ebbb6441ef5b5488
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-19 23:52:49 +0530

    SecondHigh

---
+++ src/com/test/array/SecondHigest2.java
@@ -0,0 +1,23 @@
+package com.test.array;
+
+public class SecondHigest2 {
+
+	public static void main(String[] args) {
+		int a[] = {1,3,4,5,8};
+		int result = secondHigest(a);
+		
+
+	}
+
+	private static int secondHigest(int[] a) {
+		int highest = Integer.MIN_VALUE;
+		int SecondHigest = Integer.MIN_VALUE;
+		for(int i=0; i<a.length; i++) {
+			if(a[i]<highest) {
+				
+			}
+		}
+		return 0;
+	}
+
+}
