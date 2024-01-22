22657ff80e74d5a5ff1e513b478b2e7fbd8e06c9
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-22 23:31:21 +0530

    2nd

---
+++ src/com/test/revision/SecondLargsetNumber.java
@@ -0,0 +1,16 @@
+package com.test.revision;
+
+public class SecondLargsetNumber {
+
+	public static void main(String[] args) {
+		int a[]= {1,4,3,9};
+		int res = SecondLargestNumber(a);
+
+	}
+
+	private static int SecondLargestNumber(int[] a) {
+		
+		return 0;
+	}
+
+}
