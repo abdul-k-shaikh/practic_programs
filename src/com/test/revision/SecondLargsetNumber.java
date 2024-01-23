c57ac116426b48454a7f45d3cfe696b93da2256c
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-23 22:48:33 +0530

    SecondLar

--- src/com/test/revision/SecondLargsetNumber.java
+++ src/com/test/revision/SecondLargsetNumber.java
@@ -5,12 +5,23 @@ public class SecondLargsetNumber {
 	public static void main(String[] args) {
 		int a[]= {1,4,3,9};
 		int res = SecondLargestNumber(a);
+		System.out.println("2nd largest ele :"+ res);
 
 	}
 
 	private static int SecondLargestNumber(int[] a) {
-		
-		return 0;
+		int largest = Integer.MIN_VALUE;
+		int SecondLar = Integer.MAX_VALUE;
+		for(int i=0; i<a.length; i++) {
+			if(a[i]>largest) {
+				SecondLar = largest;
+				largest = a[i];
+			}
+			else if(a[i]<largest){
+				SecondLar = a[i];
+			}
+		}
+		return SecondLar;
 	}
 
 }
