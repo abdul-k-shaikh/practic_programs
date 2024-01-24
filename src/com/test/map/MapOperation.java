b544e73d679bbe8d2d328d834399edc4292a5968
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-24 23:57:46 +0530

    mapOperation

--- src/com/test/map/MapOperation.java
+++ src/com/test/map/MapOperation.java
@@ -1,34 +1,32 @@
 package com.test.map;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class MapOperation {
-     
+
 	public static void main(String[] args) {
-		
+
 		String s = "aradhya";
-		char[] s1=s.toCharArray();
+		char[] s1 = s.toCharArray();
 		int len = s.length();
 		Map<Character, Integer> mp = new HashMap<>();
-		
-		for(int i=0; i<len; i++) {
-			if(mp.containsKey(s1[i])==false) {
+
+		for (int i = 0; i < len; i++) {
+			if (mp.containsKey(s1[i]) == false) {
 				mp.put(s1[i], 1);
-			}
-			else {
-				int oldVal=mp.get(s1[i]);
-				int newVal=oldVal+1;
+			} else {
+				int oldVal = mp.get(s1[i]);
+				int newVal = oldVal + 1;
 				mp.put(s1[i], newVal);
 			}
 		}
-		
-		Set<Map.Entry<Character, Integer>>m1 = mp.entrySet();
-		for(Map.Entry<Character,Integer>data :m1) {
-			System.out.println(data.getKey()+" "+ data.getValue());
+
+		Set<Map.Entry<Character, Integer>> m1 = mp.entrySet();
+		for (Map.Entry<Character, Integer> data : m1) {
+			System.out.println(data.getKey() + " " + data.getValue());
 		}
-		
+
 	}
 
 }
---
+++ src/com/test/map/MapPractice.java
@@ -0,0 +1,10 @@
+package com.test.map;
+
+public class MapPractice {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
