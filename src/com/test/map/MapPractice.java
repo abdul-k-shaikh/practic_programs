d91ac1b8b29e00fbcb17137f17cf310560dddd78
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-25 23:16:01 +0530

    map

--- src/com/test/map/MapPractice.java
+++ src/com/test/map/MapPractice.java
@@ -1,8 +1,21 @@
 package com.test.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapPractice {
 
 	public static void main(String[] args) {
+		String s = "Abdulislearningreactjs";
+		char[] s1 = s.toCharArray();
+		int len = s1.length;
+		
+		Map<Character, Integer>mp = new HashMap<>();
+		for(int i=0; i<len; i++) {
+			if(mp.containsKey(s1[i])==false) {
+				
+			}
+		}
 		
 
 	}
