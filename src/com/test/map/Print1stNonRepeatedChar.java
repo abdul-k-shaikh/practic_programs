22a945bf6ae99a37636c4313754a426c4288b4c4
theabdul97@gmail.com <theabdul97@gmail.com>
2024-01-28 23:43:10 +0530

    1st non repeating caracter

--- src/com/test/map/Print1stNonRepeatedChar.java
+++ src/com/test/map/Print1stNonRepeatedChar.java
@@ -7,31 +7,29 @@ import java.util.Set;
 
 public class Print1stNonRepeatedChar {
 
-		public static void main(String[] args) {
-			String str = "sillyspiders";
-			int n=6;
-			//int z[] = new int[n];
-			char a[] = str.toCharArray();
-			int len = a.length;
-			Map<Character, Integer> mp = new LinkedHashMap<>();
-			for(int i=0; i<len; i++) {
-				if(mp.containsKey(a[i])==false) {
-					mp.put(a[i], 1);
-				}
-				else {
-					int oldval=mp.get(a[i]);
-					int newVal=oldval+1;
-					mp.put(a[i], newVal);
-				}
-			}
-			Set<Map.Entry<Character, Integer>>m1 = mp.entrySet();
-			for(Map.Entry<Character,Integer>ele:m1) {
-				if(ele.getValue()==1 ) {
-					System.out.println(ele.getKey());
-					System.exit(0);
-				}
+	public static void main(String[] args) {
+		String str = "sillyspiders";
+		int n = 6;
+		// int z[] = new int[n];
+		char a[] = str.toCharArray();
+		int len = a.length;
+		Map<Character, Integer> mp = new LinkedHashMap<>();
+		for (int i = 0; i < len; i++) {
+			if (mp.containsKey(a[i]) == false) {
+				mp.put(a[i], 1);
+			} else {
+				int oldval = mp.get(a[i]);
+				int newVal = oldval + 1;
+				mp.put(a[i], newVal);
 			}
 		}
-
+		Set<Map.Entry<Character, Integer>> m1 = mp.entrySet();
+		for (Map.Entry<Character, Integer> ele : m1) {
+			if (ele.getValue() == 1) {
+				System.out.println(ele.getKey());
+				System.exit(0);
+			}
+		}
 	}
 
+}
---
+++ src/com/test/map/Print1stNonRepeatedPractice.java
@@ -0,0 +1,33 @@
+package com.test.map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Print1stNonRepeatedPractice {
+
+	public static void main(String[] args) {
+		String str = "sillyspiders";
+		char a [] = str.toCharArray();
+		Map<Character, Integer>mp = new LinkedHashMap<>();
+		int len = a.length;
+		for(int i=0; i<len; i++) {
+			if(mp.containsKey(a[i])==false) {
+				mp.put(a[i],1);
+			}
+			else {
+				int oldVal = mp.get(a[i]);
+				int newVal = oldVal+1;
+				mp.put(a[i], newVal);
+			}
+		}
+		for(Map.Entry<Character, Integer> entry : mp.entrySet()) {
+//			System.out.println(entry.getKey()+" : "+ entry.getValue());
+			if(entry.getValue()==1) {
+				System.out.println("1st non repeating caracter :"+entry.getKey());
+				System.exit(0);
+			} 
+		}
+
+	}
+
+}
