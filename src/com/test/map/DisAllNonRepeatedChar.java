08e07b2d64138634b6527d448972b3e4850680d2
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-01 23:40:58 +0530

    mpPractice

---
+++ src/com/test/map/DisAllNonRepeatedChar.java
@@ -0,0 +1,11 @@
+package com.test.map;
+
+public class DisAllNonRepeatedChar {
+
+	public static void main(String[] args) {
+		String str = "sillyspiders";
+		char a[] = str.toCharArray()
+
+	}
+
+}
