66314b827768247823cbb569b4b2555f440a1649
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-03 23:51:26 +0530

    update

---
+++ src/com/test/array/SecondLargest.java
@@ -0,0 +1,15 @@
+package com.test.array;
+
+public class SecondLargest {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5};
+		int len = a.length;
+		
+		for(int i=0; i<len; i++) {
+			
+		}
+
+	}
+
+}
