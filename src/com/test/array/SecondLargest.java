982b4b34bb6c82cc431f7bcad605f73133b90018
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-04 22:39:29 +0530

    SecondLargset

--- src/com/test/array/SecondLargest.java
+++ src/com/test/array/SecondLargest.java
@@ -3,13 +3,27 @@ package com.test.array;
 public class SecondLargest {
 
 	public static void main(String[] args) {
-		int a[] = {1,2,3,4,5};
+		int a[] = {1,2,3,4,5,8};
 		int len = a.length;
-		
+		int res = SecHighest(a);
+		System.out.println(res);
+	}
+
+	private static int SecHighest(int[] a) {
+		int len = a.length;
+		int Higest = Integer.MIN_VALUE;
+		int SecondHigest = Integer.MIN_VALUE;
 		for(int i=0; i<len; i++) {
+			if(a[i]>Higest) {
+				SecondHigest = Higest;
+				Higest = a[i];
+			}
+			else if(a[i]<Higest){
+				SecondHigest=a[i];
+			}
 			
 		}
-
+		return SecondHigest;
 	}
 
 }
