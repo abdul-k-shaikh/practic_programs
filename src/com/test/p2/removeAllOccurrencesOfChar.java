24747b1ab3052a1f06a3563f962a88ddc6bd86d5
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-07 23:33:29 +0530

    removeAllChar

---
+++ src/com/test/p2/removeAllOccurrencesOfChar.java
@@ -0,0 +1,11 @@
+package com.test.p2;
+
+public class removeAllOccurrencesOfChar {
+
+	public static void main(String[] args) {
+		String str1 = "abcdABCDabcdABCD";
+		str1 = str1.replace("a", "");
+		System.out.println(str1);
+	}
+
+}
