e3ab50ad5d8add9df911de6a7aa64940f568ff00
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-12 23:12:04 +0530

    immutable

---
+++ src/com/test/p2/immutableTest.java
@@ -0,0 +1,12 @@
+package com.test.p2;
+
+public class immutableTest {
+
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String s2 = s1;
+		System.out.println("s1==s2 :"+ s1==s2);
+
+	}
+
+}
