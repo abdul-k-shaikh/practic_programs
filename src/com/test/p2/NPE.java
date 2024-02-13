961f884eef19be1557453d59a0e0a58d1b3155d5
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-13 23:08:47 +0530

    npe

---
+++ src/com/test/p2/NPE.java
@@ -0,0 +1,17 @@
+package com.test.p2;
+
+public class NPE {
+
+	public static void main(String[] args) {
+		printString(null, 3);
+
+	}
+
+	private static void printString(String s, int count) {
+		for(int i=0; i<count; i++) {
+			System.out.println(s.toUpperCase());
+		}
+		
+	}
+
+}
