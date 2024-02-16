882e7b124d8ec9daeb9b54bab557945d033efef1
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-16 23:41:01 +0530

    TryCatch

--- src/com/test/p2/Diamond.java
+++ src/com/test/p2/Diamond.java
@@ -1,9 +1,19 @@
 package com.test.p2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Diamond {
 
 	public static void main(String[] args) {
-		//dimaond example 
+		// try-catch
+		try {
+			FileInputStream fis = new FileInputStream("Test.txt");
+			System.out.println("no file found");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Error at file");
+		}
 
 	}
 
