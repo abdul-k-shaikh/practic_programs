3403eadc09c284d4bce04488e89e48d433579f0f
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-27 23:58:02 +0530

    multilineJava15

---
+++ src/com/test/p2/TextBlock1.java
@@ -0,0 +1,10 @@
+package com.test.p2;
+
+public class TextBlock1 {
+
+	public static void main(String[] args) {
+		String textBlock = """"Hi Hello yes""";
+				System.out.println(textBlock);
+	}
+
+}
