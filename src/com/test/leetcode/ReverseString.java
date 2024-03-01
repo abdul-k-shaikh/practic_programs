851803db47657fbbb9457f184d12b449bf18c133
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-01 07:45:20 +0530

    RevString

--- src/com/test/leetcode/ReverseString.java
+++ src/com/test/leetcode/ReverseString.java
@@ -3,8 +3,23 @@ package com.test.leetcode;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		
+	   char[]  s = {'a','b','c'};
+	   
+	   revString(s);
 
+	}
+  // leetcode 344
+	private static void revString(char[] s) {
+		int start = 0, end=s.length;
+		while(start<end) {
+			char temp = s[start];
+			s[start]=s[end];
+			s[end]=temp;
+			start++;
+			end--;
+			
+		}
+		
 	}
 
 }
--- src/com/test/string/Panagram.java
+++ src/com/test/string/Panagram.java
@@ -25,7 +25,5 @@ public class Panagram {
 			}
 		}
 		System.out.println("Panagram");
-
 	}
-
 }
