39dedf3acea20bb19f95453fdc8f422e64180e67
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-12 23:38:13 +0530

    arm

--- src/com/test/p2/ArmstrongNumber.java
+++ src/com/test/p2/ArmstrongNumber.java
@@ -3,7 +3,17 @@ package com.test.p2;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 153;
+		int sum = 0;
+		while(n>0) {
+			n=n%10;
+			 sum = sum+n*n*n;
+			 n = n/10;
+			
+		}
+		if(sum==n) {
+			System.out.println("Arm");
+		}
 
 	}
 
