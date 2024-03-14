ecdce970b968d561ed81c56595401bbaefa2e933
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-14 23:57:29 +0530

    ARM

--- src/com/test/p2/ArmstrongNumber.java
+++ src/com/test/p2/ArmstrongNumber.java
@@ -6,10 +6,9 @@ public class ArmstrongNumber {
 		int n = 153;
 		int sum = 0;
 		while(n>0) {
-			n=n%10;
+			 n=n%10;
 			 sum = sum+n*n*n;
-			 n = n/10;
-			
+			 n = n/10;	
 		}
 		if(sum==n) {
 			System.out.println("Arm");
