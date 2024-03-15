c0e7d1f367e19e0be57d3b796ea7394852868d56
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-15 23:57:07 +0530

    armbw

--- src/com/test/p2/ArmstrongNumber.java
+++ src/com/test/p2/ArmstrongNumber.java
@@ -3,15 +3,19 @@ package com.test.p2;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		int n = 153;
+		int n = 371;
+		int temp = n;
 		int sum = 0;
-		while(n>0) {
+		while(n!=0) {
 			 n=n%10;
 			 sum = sum+n*n*n;
 			 n = n/10;	
 		}
 		if(sum==n) {
-			System.out.println("Arm");
+			System.out.println(temp + " Arm");
+		}
+		else {
+			System.out.println( temp+" is not an Armstrong number");
 		}
 
 	}
---
+++ src/com/test/p2/armNumber.java
@@ -0,0 +1,10 @@
+package com.test.p2;
+
+public class armNumber {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
