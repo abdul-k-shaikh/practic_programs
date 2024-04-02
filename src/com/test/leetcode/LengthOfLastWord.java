fa4ab30a7b14e1566cc6aedbe247b21b7b44408e
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-02 23:56:32 +0530

    lengthOfTheLastWord

--- src/com/test/leetcode/LengthOfLastWord.java
+++ src/com/test/leetcode/LengthOfLastWord.java
@@ -3,8 +3,15 @@ package com.test.leetcode;
 public class LengthOfLastWord {
 
 	public static void main(String[] args) {
-		
-
+		 String s = "Hello World";
+		 String a[] = s.split(" ");
+		 int len = a.length;
+		 System.out.println(len);
+		 for(int i=0; i<a.length; i++) {
+			 
+//			 System.out.println(s.length());
+//			 System.out.println(a[i]);
+			 System.out.println(a[a.length-1].length());
+		 }
 	}
-
 }
