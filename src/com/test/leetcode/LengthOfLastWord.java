1859f481a909a6ece3d6fc38841b9be207140f0f
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-03 23:46:08 +0530

    addBinary

--- src/com/test/leetcode/LengthOfLastWord.java
+++ src/com/test/leetcode/LengthOfLastWord.java
@@ -6,12 +6,13 @@ public class LengthOfLastWord {
 		 String s = "Hello World";
 		 String a[] = s.split(" ");
 		 int len = a.length;
-		 System.out.println(len);
-		 for(int i=0; i<a.length; i++) {
-			 
-//			 System.out.println(s.length());
-//			 System.out.println(a[i]);
-			 System.out.println(a[a.length-1].length());
-		 }
+//		 System.out.println(a[len-1]);
+		 System.out.println(a[len-1].length());
+//		 for(int i=0; i<a.length; i++) {
+//			 
+////			 System.out.println(s.length());
+////			 System.out.println(a[i]);
+//			 System.out.println(a[a.length-1].length());
+//		 }
 	}
 }
---
+++ src/com/test/leetcode/addBinary.java
@@ -0,0 +1,10 @@
+package com.test.leetcode;
+
+public class addBinary {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
