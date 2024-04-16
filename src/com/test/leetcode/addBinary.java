140a6522ee8a168ed804a8642eb8701e9b556a13
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-16 23:29:40 +0530

    addBinarySolution

--- src/com/test/leetcode/addBinary.java
+++ src/com/test/leetcode/addBinary.java
@@ -3,8 +3,20 @@ package com.test.leetcode;
 public class addBinary {
 
 	public static void main(String[] args) {
-		
+		System.out.println(addBinary("1111","1010"));
 
+	}
+
+	private static String addBinary(String a, String b) {
+		int n1=a.length()-1;
+		int n2=a.length()-1;
+		int c=0, base=2;
+		StringBuilder sb = new StringBuilder();
+		while(n1)
+		
+		
+		
+		return null;
 	}
 
 }
