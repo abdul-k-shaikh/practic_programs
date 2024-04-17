bb45a07fa4903579d1a4ee588374cee7ee4808b1
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-17 23:37:23 +0530

    addBinary

--- src/com/test/leetcode/addBinary.java
+++ src/com/test/leetcode/addBinary.java
@@ -11,12 +11,28 @@ public class addBinary {
 		int n1=a.length()-1;
 		int n2=a.length()-1;
 		int c=0, base=2;
-		StringBuilder sb = new StringBuilder();
-		while(n1)
+		StringBuilder res = new StringBuilder();
+		while(n1>=0 || n2>=0)
+		{
+			int t1=0, t2=0, sum;
+			if(n1>=0)
+				t1=a.charAt(n1--)-'0';// ASCII Value 0-
+			if(n2>=0)
+				t2=b.charAt(n2--)-'0';
+			sum=t1+t2;
+			if(sum>=base) {
+				c=1;
+				sum=sum-base;
+			}
+			else
+				c=0;
+			    res.append(sum);
+		}
+		if(c==1)
+			res.append(c);
 		
 		
-		
-		return null;
+		return res.reverse().toString();
 	}
 
 }
