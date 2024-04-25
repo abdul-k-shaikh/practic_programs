f53875db10f0d3f845be766b1a03fcb5a7087a17
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-25 19:48:43 +0530

    reverseVowelsOfString

--- src/com/test/leetcode/ReverseVowelsOfString345.java
+++ src/com/test/leetcode/ReverseVowelsOfString345.java
@@ -3,8 +3,43 @@ package com.test.leetcode;
 public class ReverseVowelsOfString345 {
 
 	public static void main(String[] args) {
-		
-
+		String s="hello";
+		String result = revVowels(s);
+		System.out.println("rev vowels is :" + result );
 	}
+
+	private static String revVowels(String s) {
+		int start=0, end=s.length()-1;
+		char c[] = s.toCharArray();
+		
+		while(start<end) {
+			if(!isVowel(c[start])) {
+				start++;
+			}
+			else if(!isVowel(c[end])) {
+				end--;
+			}
+			else {
+				char temp=c[end];
+				c[end]=c[start];
+				c[start]=temp;
+				start++;
+				end--;
+				
+			}
+		}
+		
+		return String.valueOf(c);
+	}
+
+	private static boolean isVowel(char c) {
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='e'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+			return true;
+		}
+		return false;
+		
+	}
+
+	
 
 }
