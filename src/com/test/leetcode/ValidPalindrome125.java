d25eaa83f5e88352e6c8b41078f20a3962110152
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-28 19:55:34 +0530

    LazyAndEagarInitialization

--- src/com/test/leetcode/ValidPalindrome125.java
+++ src/com/test/leetcode/ValidPalindrome125.java
@@ -4,7 +4,21 @@ public class ValidPalindrome125 {
 
 	public static void main(String[] args) {
 		
+		String s = "race a car";
+		System.out.println(isPalindrome(s));
+	}
 
+	private static boolean isPalindrome(String s) {
+		int start =0, end=s.length()-1;
+		s=s.toLowerCase().replaceAll("[^A-Za-z0-9]", s);
+		while(start<=end) {
+			if(s.charAt(start)!=s.charAt(end)) {
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
 	}
 
 }
