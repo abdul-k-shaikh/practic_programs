a4645c543a97c45bfae66fabb41701a5ba28690c
theabdul97@gmail.com <theabdul97@gmail.com>
2024-04-29 23:56:00 +0530

    ValidPalindromeTest

--- src/com/test/leetcode/ValidPalindromeTest.java
+++ src/com/test/leetcode/ValidPalindromeTest.java
@@ -1,19 +1,17 @@
 package com.test.leetcode;
 
-public class ValidPalindrome125 {
+public class ValidPalindromeTest {
 
 	public static void main(String[] args) {
-		
-		String s = "race a car";
-		System.out.println(isPalindrome(s));
+		String s = "A man, a plan, a canal: Panama";
+		System.out.println(isValidPalindrome(s));
 	}
-
-	private static boolean isPalindrome(String s) {
+	private static boolean isValidPalindrome(String s) {
 		int start=0, end=s.length()-1;
 		s=s.toLowerCase().replaceAll("[^A-Za-z0-9]", s);
 		while(start<=end) {
 			if(s.charAt(start)!=s.charAt(end)) {
-				return false;
+			return false;
 			}
 			start++;
 			end--;
