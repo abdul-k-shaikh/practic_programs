b1fc26bcb23e2735ff3e19d7f0d3fdfa5771116f
theabdul97@gmail.com <theabdul97@gmail.com>
2024-05-02 23:14:03 +0530

    marks

---
+++ src/com/test/iTransform/MarksCalculation.java
@@ -0,0 +1,16 @@
+package com.test.iTransform;
+
+import java.util.Scanner;
+
+public class MarksCalculation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter marks for three subjects:");
+		int subject1 = sc.nextInt();
+		int subject2 = sc.nextInt();
+		int subject3 = sc.nextInt();
+		
+	}
+
+}
