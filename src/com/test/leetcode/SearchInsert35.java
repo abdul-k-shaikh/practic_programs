b1548e29b0879d8f4922a8d12c0a7542387f4f1a
theabdul97@gmail.com <theabdul97@gmail.com>
2024-05-04 23:52:11 +0530

    SearchInsert35

---
+++ src/com/test/leetcode/SearchInsert35.java
@@ -0,0 +1,16 @@
+package com.test.leetcode;
+
+public class SearchInsert35 {
+
+	public static void main(String[] args) {
+		int nums[] = {1,3,5,6,9};
+		int target=5;
+		searchInsert(nums,target);
+		
+	}
+
+	private static void searchInsert(int[] nums, int target) {
+				
+	}
+
+}
