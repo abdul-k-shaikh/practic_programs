29ad0563ba13327c4f74374d36273b7166d038d8
theabdul97@gmail.com <theabdul97@gmail.com>
2024-05-05 22:56:37 +0530

    SearchInsert35

--- src/com/test/leetcode/SearchInsert35.java
+++ src/com/test/leetcode/SearchInsert35.java
@@ -4,12 +4,28 @@ public class SearchInsert35 {
 
 	public static void main(String[] args) {
 		int nums[] = {1,3,5,6,9};
-		int target=5;
-		searchInsert(nums,target);
+		int target=4;
+		System.out.println(searchInsert(nums,target));
 		
 	}
 
-	private static void searchInsert(int[] nums, int target) {
+	private static int searchInsert(int[] nums, int target) {
+		int low = 0;
+		int high=nums.length-1;
+		while(low<=high) {
+			int mid = low+(high-low)/2;
+			if(nums[mid]==target) {
+				return mid;
+			}
+			else if(nums[mid]>target) {
+				high=mid-1;
+			}
+			else {
+				low=mid+1;
+			}
+			
+		}
+		return low;
 				
 	}
 
