ff4bf9814975da2659b23660e6373a8eba3e0402
theabdul97@gmail.com <theabdul97@gmail.com>
2024-05-26 00:15:23 +0530

    CharCount

--- src/com/test/iwM24/CharCount5.java
+++ src/com/test/iwM24/CharCount5.java
@@ -1,10 +1,26 @@
 package com.test.iwM24;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class CharCount5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = " Abdul is working";
+		str = str.replace(" ", "");
+		int len=str.length();
+		Map<Character, Integer>mp = new LinkedHashMap<>();
+		
+		char ch;
+		for(int i=0; i<len; i++) {
+			ch=str.charAt(i);
+			mp.put(ch, mp.containsKey(ch)?mp.get(ch)+1: 1);
+			
+		}
+		
+		for(Map.Entry<Character, Integer>m1:mp.entrySet()) {
+			System.out.println(m1.getKey()+":"+m1.getValue());
+		}
 	}
 
 }
