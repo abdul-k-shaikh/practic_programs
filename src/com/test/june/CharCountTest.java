b12240ad2c92624f1737c2cbf15ce29a54920590
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-03 22:56:39 +0530

    CharCount1

---
+++ src/com/test/june/CharCountTest.java
@@ -0,0 +1,23 @@
+package com.test.june;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharCountTest {
+
+	public static void main(String[] args) {
+		String str = "Abdul is working on java";
+		str= str.replaceAll(" ","");
+		Map<Character, Integer>mp = new LinkedHashMap<>();
+		char ch;
+		for(int i=0;i<str.length();i++) {
+			ch = str.charAt(i);
+			mp.put(ch, mp.containsKey(ch)?mp.get(ch)+1:1);
+			
+		}
+		for(Map.Entry<Character, Integer>m1:mp.entrySet()) {
+			System.out.println(m1.getKey()+":"+m1.getValue());
+		}
+	}
+
+}
