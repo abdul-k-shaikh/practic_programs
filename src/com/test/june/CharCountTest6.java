69a8449c80e679aa8b9c230e816a7b8ac1d35911
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-20 18:21:48 +0530

    charCount

---
+++ src/com/test/june/CharCountTest6.java
@@ -0,0 +1,26 @@
+package com.test.june;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharCountTest6 {
+
+	public static void main(String[] args) {
+		String str = "Abdul is working";
+		str = str.replaceAll(" ", "");
+		System.out.println(str);
+		
+		Map<Character, Integer>mp = new LinkedHashMap<>();
+		char ch;
+		for(int i=0; i<str.length(); i++) {
+			ch=str.charAt(i);
+			mp.put(ch, mp.containsKey(ch)?mp.get(ch)+1:1);
+		}
+		
+		for(Map.Entry<Character, Integer>m1:mp.entrySet()) {
+			System.out.println(m1.getKey()+":"+ m1.getValue());
+		}
+			
+	}
+
+}
