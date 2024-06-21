5000c179bd3ccf52f5fda049e46c3dcb9382aa8b
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-21 23:48:52 +0530

    shiftedChar

---
+++ src/com/test/Test12.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		String input = "ABCD";
+		StringBuilder output = new StringBuilder();
+		
+		for(char c : input.toCharArray()) {
+			char shiftedChar = (char)(c+1);
+			output.append(shiftedChar);
+		}
+		System.out.println("Input: " + input + ", Output: " + output);
+	}
+
+}
---
+++ src/com/test/intTest/Parent.java
@@ -0,0 +1,7 @@
+package com.test.intTest;
+
+public interface Parent {
+		 default int add();
+		
+		
+}
