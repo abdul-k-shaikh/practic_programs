2feb5cfbb749c4fea7a9d188e566f6d5e21272e5
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-24 09:37:41 +0530

    shiftedChar

--- src/com/test/june/ShiftedCharTest1.java
+++ src/com/test/june/ShiftedCharTest1.java
@@ -3,7 +3,14 @@ package com.test.june;
 public class ShiftedCharTest1 {
 
 	public static void main(String[] args) {
-
+		String input = "ABCD";
+		StringBuilder output = new StringBuilder();
+		
+		for(char c : input.toCharArray()) {
+			char shiftedChar = (char) (c+1);
+			output.append(shiftedChar);
+		}
+		System.out.println("input is : "+input +" and"+ "output is : "+output);
 	}
 
 }
