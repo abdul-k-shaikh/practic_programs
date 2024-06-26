3ff0411503769fd10dbd35778234b7a81f685f8d
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-26 23:43:39 +0530

    ImutableProof

---
+++ src/com/test/june/StringTest.java
@@ -0,0 +1,11 @@
+package com.test.june;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+			String s1 = "Abdul";
+			String s2 = s1+"shaikh";
+			System.out.println("s2 is : "+ s2);		
+			System.out.println("s1 is : "+ s1);
+	}
+}
---
+++ src/com/test/june/ValidBrackets.java
@@ -0,0 +1,9 @@
+package com.test.june;
+
+public class ValidBrackets {
+
+	public static void main(String[] args) {
+		String s = ""
+	}
+
+}
