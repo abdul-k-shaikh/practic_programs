11528e4d18f4548faa07211529d0b963d5bb6973
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-28 23:34:00 +0530

    charTest

---
+++ src/com/test/june/CharTest.java
@@ -0,0 +1,16 @@
+package com.test.june;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharTest {
+
+	public static void main(String[] args) {
+		String str = "Abdul is working";
+		str= str.replaceAll(" ", "");
+		System.out.println(str);
+		Map<Character,Integer>map = new LinkedHashMap<>();
+		
+	}
+
+}
