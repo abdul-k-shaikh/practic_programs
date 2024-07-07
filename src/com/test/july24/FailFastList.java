b3ab033bfca44d96c8d1a1116e2d6a3804f8489f
theabdul97@gmail.com <theabdul97@gmail.com>
2024-07-07 08:41:23 +0530

    sortHashmaoByValue

--- src/com/test/july24/FailFastList.java
+++ src/com/test/july24/FailFastList.java
@@ -5,20 +5,18 @@ import java.util.Iterator;
 import java.util.List;
 
 public class FailFastList {
-
+	// Concurrent Modification Exception example
 	public static void main(String[] args) {
-		List<String>list = new ArrayList<>();
+		List<String> list = new ArrayList<>();
 		list.add("a");
 		list.add("b");
-		
+
 		Iterator<String> iterator = list.iterator();
-		
-		while(iterator.hasNext()) {
+
+		while (iterator.hasNext()) {
 			String element = iterator.next();
 			System.out.println(element);
 			list.add("e");
 		}
-		
 	}
-
 }
---
+++ src/com/test/july24/MapSortByValue.java
@@ -0,0 +1,15 @@
+package com.test.july24;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSortByValue {
+//Sort the hashmap by values
+	public static void main(String[] args) {
+		Map<Integer, String>map = new HashMap<>();	
+		map.put(1, "B");
+		map.put(3, "C");
+		map.put(9, "E");
+	}
+
+}
