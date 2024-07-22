f8664345bb78f4e2f74591f68a952c1edf53f738
theabdul97@gmail.com <theabdul97@gmail.com>
2024-07-22 23:54:16 +0530

    OddEven

--- src/com/test/july24/RemoveDupli.java
+++ src/com/test/july24/RemoveDupli.java
@@ -7,11 +7,21 @@ import java.util.stream.Collectors;
 public class RemoveDupli {
 
 	public static void main(String[] args) {
-		String str[] = {"java", "c", "cpp", "java"};
+		String str[] = { "java", "c", "cpp", "java" };
+		// 1. Remove duplicates from the given array
 		List<String> asList = Arrays.asList(str);
 		List<String> collect = asList.stream().distinct().collect(Collectors.toList());
 		System.out.println(collect);
 		
+		//2.Given list of integers separate odd n even 
+		int a[] = {1,2,3,4,5,6,7,8,};
+		List<int[]> asList2 = Arrays.asList(a);
+		asList2.stream().collect(Collectors.partitioningBy(a->a%2==0));
+		
+		
+		
+		
+
 	}
 
 }
