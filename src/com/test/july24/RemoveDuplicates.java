a79c918b2edf066192c4b9b04502451a5e12f23b
theabdul97@gmail.com <theabdul97@gmail.com>
2024-07-29 23:25:02 +0530

    CountOfEachElement

--- src/com/test/july24/RemoveDuplicates.java
+++ src/com/test/july24/RemoveDuplicates.java
@@ -2,6 +2,8 @@ package com.test.july24;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RemoveDuplicates {
@@ -11,7 +13,10 @@ public class RemoveDuplicates {
 
 		List<Integer> collect = a.stream().distinct().collect(Collectors.toList());
 		System.out.println(collect);
-
+		
+		List<String> stationeryList = Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Stapler", "Note Book", "Pencil");
+		Map<String, Long> stationeryCountMap = stationeryList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println(stationeryCountMap);
 	}
 
 }
