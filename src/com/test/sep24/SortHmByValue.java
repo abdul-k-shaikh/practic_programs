720bb2d68f2c8c56efea908d2cd8525121da4851
theabdul97@gmail.com <theabdul97@gmail.com>
2024-09-14 00:52:28 +0530

    SortHashMapByValues

--- src/com/test/sep24/SortHmByValue.java
+++ src/com/test/sep24/SortHmByValue.java
@@ -1,9 +1,30 @@
 package com.test.sep24;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class SortHmByValue {
 
 	public static void main(String[] args) {
+		Map<String, Integer> mp = new HashMap<String, Integer>();
+		mp.put("a", 3);
+		mp.put("b", 2);
+		mp.put("c", 1);
+		mp.put("e", 6);
+		mp.put("f", 9);
 
+		List<Integer> list = mp.values().stream().sorted().collect(Collectors.toList());
+		System.out.println("mapValues" + list);
+
+		for (int ele : list) {
+			for (String key : mp.keySet()) {
+				if (mp.get(key).equals(ele)) {
+					System.out.println(key + ":" + ele);
+				}
+			}
+		}
 	}
 
 }
